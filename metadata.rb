2a870fea935b16aa556436c490a9868df30479d6
Kevin J. Dickerson <kevin.dickerson@gmail.com>
2014-10-31 15:42:53 -0700

    add initial cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'wfpblife'
+maintainer       'Kevin J. Dickerson'
+maintainer_email 'kevin.dickerson@loom.technology'
+license          'Apache 2.0'
+description      'Installs/Configures wfpblife'
+long_description 'Installs/Configures wfpblife'
+version          '0.1.0'
+
