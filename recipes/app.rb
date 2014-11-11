ab3658cb08351bc5e7ffaaf886d1898074833d71
Kevin J. Dickerson <kevin.dickerson@gmail.com>
2014-11-11 14:16:03 -0800

    Add wp-cli, some php modules, cron for backups, and more.

--- recipes/app.rb
+++ recipes/app.rb
@@ -19,24 +19,60 @@
 
 dbs = search(:node, 'roles:wfpblife-db')
 
-unless dbs && dbs.first['fqdn']
-  fail 'Could not find a database server in the node list.'
-end
+bag = {}
+bag[:db] = data_bag_item('wp', 'db')
+bag[:setup] = data_bag_item('wp', 'setup')
+bag[:w3tc] = data_bag_item('wp', 'w3tc')
 
-unless dbs.count == 1
-  fail "There are #{dbs.count} database servers instead of exactly one. There should be only one databse server."
-end
+fail 'Could not find a database server in the node list.' unless dbs && dbs.first['fqdn']
 
-wp_db = data_bag_item('wp', 'db')
+fail "There should be exactly 1 database node, but there are #{dbs.count}." unless dbs.count == 1
 
-node.override[:wordpress][:db][:pass] = wp_db['password']
+fail "Required data bag wp::setup is not configured correctly. #{bag[:setup].to_json}" \
+  unless bag[:setup] &&
+         bag[:setup]['domain'] &&
+         bag[:setup]['title'] &&
+         bag[:setup]['admin_user'] &&
+         bag[:setup]['admin_password'] &&
+         bag[:setup]['admin_email']
+
+fail "Required data bag wp::w3tc is not configured correctly. #{bag[:w3tc].to_json}" unless bag[:w3tc]
+
+node.override[:wordpress][:db][:pass] = bag[:db]['password']
 node.default[:wordpress][:db][:host] = dbs.first['fqdn']
-node.default[:wordpress][:db][:user] = wp_db['user']
-node.default[:wordpress][:db][:name] = wp_db['name']
+node.default[:wordpress][:db][:user] = bag[:db]['user']
+node.default[:wordpress][:db][:name] = bag[:db]['name']
+
+node.default[:w3tc][:cloudfront_key] = bag[:w3tc]['cloudfront_key']
+node.default[:w3tc][:cloudfront_secret] = bag[:w3tc]['cloudfront_secret']
+node.default[:w3tc][:cloudfront_id] = bag[:w3tc]['cloudfront_id']
 
 include_recipe 'apt'
+include_recipe 'php'
+
+package 'php5-curl' do
+  action :install
+end
+
+package 'memcached' do
+  action :install
+end
+
+package 'php5-memcache' do
+  action :install
+end
+
 include_recipe 'wordpress'
 include_recipe 'wp-cli'
+
+execute 'ensure core wordpress is initialized' do
+  command <<-CMD
+    wp core install --url="#{bag[:setup]['domain']}" --title="#{bag[:setup]['title']}" --admin_user="#{bag[:setup]['admin_user']}" --admin_password="#{bag[:setup]['admin_password']}" --admin_email="#{bag[:setup]['admin_email']}"
+  CMD
+  cwd node[:wordpress][:dir]
+  user node[:apache][:user]
+  action :run
+end
 
 directory "#{node[:wordpress][:dir]}/health_check" do
   owner node['apache']['user']
@@ -49,4 +85,45 @@ template "#{node[:wordpress][:dir]}/health_check/index.php" do
   group node['apache']['user']
   source 'health_check.php.erb'
   mode '0644'
+end
+
+execute 'install the w3-total-cache plugin' do
+  command 'wp plugin install w3-total-cache'
+  cwd node[:wordpress][:dir]
+  user node[:apache][:user]
+  action :run
+end
+
+execute 'activate the w3-total-cache plugin' do
+  command 'wp plugin activate w3-total-cache'
+  cwd node[:wordpress][:dir]
+  user node[:apache][:user]
+  action :run
+end
+
+directory "#{node[:wordpress][:dir]}/wp-content/w3tc-config" do
+  owner node['apache']['user']
+  group node['apache']['user']
+  recursive true
+end
+
+template "#{node[:wordpress][:dir]}/wp-content/w3tc-config/master.php" do
+  owner node['apache']['user']
+  group node['apache']['user']
+  source 'w3tc_config_master.php.erb'
+  mode '0644'
+end
+
+template "#{node[:wordpress][:dir]}/wp-content/w3tc-config/master-admin.php" do
+  owner node['apache']['user']
+  group node['apache']['user']
+  source 'w3tc_config_master_admin.php.erb'
+  mode '0644'
+end
+
+file "#{node[:wordpress][:dir]}/wp-content/w3tc-config/index.html" do
+  owner node['apache']['user']
+  group node['apache']['user']
+  mode '0644'
+  action :create
 end
--- recipes/db.rb
+++ recipes/db.rb
@@ -17,9 +17,11 @@
 # limitations under the License.
 #
 
-wp_db = data_bag_item('wp', 'db')
+bag = {}
+bag[:db] = data_bag_item('wp', 'db')
+bag[:mysql_aws_backup] = data_bag_item('mysql_aws_backup', 'aws_credentials')
 
-node.override['mysql']['server_root_password'] = wp_db['root_password']
+node.override['mysql']['server_root_password'] = bag[:db]['root_password']
 node.override['mysql']['service_name'] = 'wfpblife_mysql'
 
 include_recipe 'apt'
@@ -30,16 +32,16 @@ db_credentials = { host: '127.0.0.1', port: 3306, username: 'root',
                    password: node['mysql']['server_root_password'] }
 
 # Create a mysql database
-mysql_database wp_db['name'] do
+mysql_database bag[:db]['name'] do
   connection db_credentials
   action :create
 end
 
 # grant all privileges on all databases/tables from any address
-mysql_database_user wp_db['user'] do
+mysql_database_user bag[:db]['user'] do
   connection db_credentials
-  password wp_db['password']
-  database_name wp_db['name']
+  password bag[:db]['password']
+  database_name bag[:db]['name']
   host '%'
   action :grant
 end
@@ -57,4 +59,59 @@ template '/etc/mysql/conf.d/wfpblife.cnf' do
   owner 'mysql'
   group 'mysql'
   notifies :restart, 'mysql_service[wfpblife_mysql]'
+end
+
+gem_package 'aws-sdk' do
+  version '1.57.0'
+  action :install
+end
+
+gem_package 'resque' do
+  action :install
+end
+
+gem_package 'mysql-aws-backup' do
+  action :install
+end
+
+directory "/var/log/mysql-aws-backup" do
+  owner 'root'
+  group 'root'
+  recursive true
+end
+
+user 'worker' do
+  comment 'e.g. for cron jobs'
+  system true
+  home '/home/worker'
+  supports manage_home: true
+  action :create
+end
+
+file "/var/log/mysql-aws-backup/log.txt" do
+  owner 'worker'
+  group 'worker'
+  mode '0755'
+  action :create
+end
+
+mysql_aws_backup_env = { MYSQL_AWS_BACKUP_HOST:'127.0.0.1',
+                         MYSQL_AWS_BACKUP_MYSQL_USER:'root',
+                         MYSQL_AWS_BACKUP_MYSQL_PASSWORD:node['mysql']['server_root_password'],
+                         MYSQL_AWS_BACKUP_FILE_PREFIX:'wfpblife-backup',
+                         MYSQL_AWS_BACKUP_BUCKET:'wfpblife-db-backups',
+                         MYSQL_AWS_BACKUP_DATABASE:bag[:db]['name'],
+                         MYSQL_AWS_BACKUP_AWS_ACCESS_KEY_ID:bag[:mysql_aws_backup]['aws_id'],
+                         MYSQL_AWS_BACKUP_AWS_SECRET_ACCESS_KEY:bag[:mysql_aws_backup]['aws_secret'],
+                         MYSQL_AWS_BACKUP_AWS_REGION:bag[:mysql_aws_backup]['aws_region'] }
+
+cron 'mysql_aws_backup' do
+  action :create
+  user 'worker'
+  minute '0'
+  hour '2'
+  weekday '*'
+  home '/home/worker'
+  environment mysql_aws_backup_env
+  command '/opt/chef/embedded/bin/mysql-aws-backup 2>&1 | tee --append /var/log/mysql-aws-backup/log.txt'
 end
